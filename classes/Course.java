ef5ae0c522d9553084b80fa0f6cbd54f03474c50
Kenny Oh <koh@appnexus.com>
2016-04-19 20:22:10 -0400

    correcting merge conflict

--- classes/Course.java
+++ classes/Course.java
@@ -1,26 +1,14 @@
 public class Course {
-<<<<<<< HEAD
+
 	private int department;
 	private int courseNum;
 	private String name;
 	private char day;
 	private int timeSlot; // this should be pulled from period object i think 
 	private int credits;
-	private String roster[]=new String [20]; //array language
-
-	public void Course (int department, int courseNum, String name, char day, int timeSlot, int credits) {
-=======
-
-  private int department;
-  private int courseNum;
-  private String name;
-  private char day;
-  private int timeSlot; // this should be pulled from period object i think
-  private int credits;
-  private int [] roster; //array language
+	private String roster[] = new String [20]; //array language
 
   public void Course(int department, int courseNum, String name, char day, int timeSlot, int credits) {
->>>>>>> refs/remotes/origin/master
     this.department = department;
     this.courseNum = courseNum;
     this.name = name;
@@ -48,17 +36,9 @@ public class Course {
   public int getCredits() {
     return credits;
   }
-<<<<<<< HEAD
+
   public Student[] getRoster(){
     return roster;
   }
-  /* need public String toString() and public boolean equals ()
-  */
-=======
-
-  public Student[] getRoster() {
-    return roster;
-  }
-
->>>>>>> refs/remotes/origin/master
+  // need public String toString() and public boolean equals ()
 }
