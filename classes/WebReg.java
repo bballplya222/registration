832556f087fa1093077b2c4a06e434aa9218d1c7
Chris Presby <Cpresby1@mail.brookdalecc.edu>
2016-04-27 23:53:02 -0400

    error in drop course method check for error conditions and 3 other ocurrences

--- classes/WebReg.java
+++ classes/WebReg.java
@@ -1,11 +1,14 @@
-public class {
+package registration.classes;
+
+
+public class WebReg {
   public static Course lookupCourseByName(Course[] catalog, String courseName){
     for(int i = 0; i < catalog.length; i++){
       if (catalog[i].getName().equalsIgnoreCase(courseName)){
         return catalog[i];
       }
     }
-    return null
+    return null;
   }
   public static Course[] lookupCoursesByDept(Course[] catalog, int dept){
     int count = 0;
@@ -27,55 +30,120 @@ public class {
     }
     return a;
   }
-  public static boolean addCourse(
-    Course[] catalog,
-    Course[] mySchedule,
-    int myNumCourses,
-    int dept,
-    int courseNum)
+  public static boolean addCourse(Student s, Course c)
   {
+	  //while (s(i)!=null?
+	Course [] mySchedule=s.getSchedule();  
+	Student [] roster= c.getRoster();
     int j = 0;
-    if (mySchedule.length <= myNumCourses){
-      System.out.println(mySchedule.length);
-      return false;
-    }
-    for (int i = 0; i < catalog.length; i++){
-    if(catalog[i].getDepartment() == dept && catalog[i].getCourseNumber() == courseNum){
-      mySchedule[myNumCourses] = catalog[i];
+    for (int i = 0; i < mySchedule.length; i++){
+    if(mySchedule[i]!=null){
       j++;
       }
     }
-    if( j == 0){
+    if( j == mySchedule.length){
       return false;
     }
+    for (int i = 0; i < mySchedule.length; i++){
+        if(mySchedule[i]==null){
+        	continue; 
+        }
+        if (mySchedule[i].getPeriod().compareTo(c.getPeriod())==0){
+        	return false; 
+        }
+    }
+    j=0;
+    for (int i=0; i<roster.length;i++){
+    	if (roster[i]!=null){
+    		j++;
+    	}
+    }
+    if (j==roster.length){
+    	return false;
+    }
+    int spot= 0;
+    for (int i=0; i<roster.length; i++){
+    	if (roster [i] == null){
+    		spot=i;
+    		break;
+    	}
+    }
+    int spot1=0;
+    for (int i=0;i<mySchedule.length; i++){
+    	if(mySchedule[i]==null){
+    		spot1=i;
+    		break;
+    	}
+    }
+    mySchedule[spot1]=c;
+    roster[spot]=s;
     return true;
   }
-  public static boolean dropCourse(
-    Course[] mySchedule,
-    int myNumCourses,
-    int dept,
-    int courseNum)
+  
+  public static boolean dropCourse(Student s, Course c)
   {
-    int j = 0;
-    for(int i = 0; i < myNumCourses; i++){
-    if(mySchedule[i].getDepartment() == dept && mySchedule[i].getCourseNumber() == courseNum){
-      if(i < myNumCourses){
-        for(j = i; j < myNumCourses; j++){
-          mySchedule[i] = mySchedule[i+1];
-          i++;
-        }
-      }
+	  
+	 Course [] mySchedule= s.getSchedule();
+	 Student roster []=c.getRoster();
+    int schedspot = 0;
+    boolean not= true;
+    
+    for(int i1 = 0; i1 < mySchedule.length; i1++){
+    	if (mySchedule[i1]==c){
+    		not=false;
+    		schedspot=i1;
+    	}
     }
+    if (not==true){
+    	return false;
     }
-    if (j==0){
-    return false;
+    int rosterspot=0;
+    boolean notros=true;
+    for(int i1 = 0; i1 < roster.length; i1++){
+    	if (roster[i1]==s){
+    		not=false;
+    		rosterspot=i1;
+    	}
     }
-    return true
+    if (notros==true){
+    	return false;
+    }
+    mySchedule[schedspot]=null; 
+    roster[rosterspot]=null;// we have removed student and course from respective array
+    
+    for( int i1=0;i1<mySchedule.length;i1++){
+    	if (mySchedule[i1]==null){
+    		for(int k=i1+1; k<mySchedule.length; k++){
+    			mySchedule[k-1]=mySchedule[k];
+    		}
+    		mySchedule[mySchedule.length-1]=null;
+    		break;
+    	}
+    	mySchedule[i1]=mySchedule[i1+1];
+    }
+    for( int i1= 0;i1<roster.length;i1++){
+    	if (roster[i1]==null){
+    		for(int k=i1+1; k<roster.length; k++){
+    			roster[k-1]=roster[k];
+    		}
+    		roster[roster.length-1]=null;
+    		break;
+    	}
+    	roster[i1]=roster[i1+1];
+    }
+   
+    return true;
   }
-  public static int countCredits(Course[] mySchedule, int myNumCourses)
-  {
+
+ 
+    
+  public static int countCredits(Student s){
     int credit = 0;
-    for(int i = 0; i < myNumCourses; i++){
+    Course [] mySchedule=s.getSchedule();
+    for(int i = 0; i < mySchedule.length; i++){
+    	if(mySchedule[i]==null){
+    		break;
+    	}
       credit = credit + mySchedule[i].getCredits();
       
     }
